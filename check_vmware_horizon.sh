a9f3e8aff22d711323c5a926030c87c7c0d67a9f
F <58272808+ascii42@users.noreply.github.com>
2023-04-17 16:05:54 +0200

    Bugfixies 0.1.1
    fix the findings

--- check_vmware_horizon.sh
+++ check_vmware_horizon.sh
@@ -6,12 +6,18 @@
 #   Felix Longardt <monitoring@longardt.com>
 #
 # Version history:
-# 2022-02-10 Felix Longardt <monitoring@longardt.com>
+# 2019-08-10 Felix Longardt <monitoring@longardt.com>
 # Release: 0.0.1
 #   Initial release
+# 2022-02-10 Felix Longardt <monitoring@longardt.com>
+# Release: 0.0.2
+#   further deployment
 # 2022-02-27 Felix Longardt <monitoring@longardt.com>
 # Release: 0.1.0
 #   Add a lot of functionality, verbose output etc..
+# 2023-04-15 Felix Longardt <monitoring@longardt.com>
+# Release: 0.1.1
+#   Bugfixies help and switches
 
 ## VARIABLES
 PROGNAME="${0##*/}"
@@ -26,7 +32,7 @@ DATE="$(which date)"
 exit_unknown() {
         echo "Unknown parameter: ${1}"
         print_usage
-        exit 4 
+        exit 4
 }
 
 ## FUNCTIONS
@@ -44,6 +50,7 @@ print_help() {
         print_usage
 cat << EOM
 
+
  This plugin checks several statistics of a vmware Horizon Connection Server via API.
 
 Options:
@@ -51,9 +58,7 @@ Options:
     Print detailed help screen
  -V, --version
     Print version information
- -i, --input <integer>
-    Set input value to INTEGER percent
- -H, --hostname <hostname>
+ -H, --host, --hostname <hostname>
     Set ipadress or hostname for destination
  -U, --username <username>
     Set Username for Login
@@ -61,55 +66,55 @@ Options:
     Set Password for Login
  -D, --domain <domain>
     Set Domain for Login
- -eCS---enable-connectionserver
+ -eCS, ---enable-connectionserver
     Enable Connection Server check
- -eGW--enable-gateway
+ -eGW, --enable-gateway
     Enable Gateway check
- -eVC--enable-vcenter
+ -eVC, --enable-vcenter
     Enable vCenter check
- -eDB--enable-db
+ -eDB, --enable-db
     Enable Event DB check
- -eAD--enable-ad
+ -eAD, --enable-ad
     Enable AD-Domain check
- -eRDS--enable-rds
+ -eRDS, --enable-rds
     Enable RDS Server check
- -eFA--enable-farms
+ -eFA, --enable-farms
     Enable RDS Farms check
- -eSA--enable-saml
+ -eSA, --enable-saml
     Enable RDS Farms check
- -eTS--enable-truesso
+ -eTS, --enable-truesso
     Enable True SSO check
- -ePO--enable-pod
+ -ePO, --enable-pod
     Enable POD check
- -A|--enable-all
+ -A, --enable-all
     Enable all available checks.
  -w, --warning <integer>
-    Set WARNING status for sessions 
+    Set WARNING status for sessions
     if --enable-all ist set, warning will match on all sessions.
  -c, --critical <integer>
-    Set CRITICAL status for sessions 
+    Set CRITICAL status for sessions
     if --enable-all ist set, critical will match on all sessions.
- -wCtS|--warning-cs-total-session  <integer>
+ -wCtS, --warning-cs-total-session  <integer>
     Set if you want to enable Warning on Connection Server total sessions.
     This will overwrite -w --warning
- -cCtS|--critical-cs-total-session  <integer>
+ -cCtS, --critical-cs-total-session  <integer>
     Set if you want to enable Critical on Connection Server total sessions.
     This will overwrite -c --critical <integer>
- -wCcS|--warning-cs-current-session <integer>
+ -wCcS, --warning-cs-current-session <integer>
     Set if you want to enable Warning on Connection Server current sessions.
     This will overwrite -w --warning
- -cCcS|--critical-cs-current-session <integer>
+ -cCcS, --critical-cs-current-session <integer>
     Set if you want to enable Critical on Connection Server current sessions.
     This will overwrite -c --critical
- -wGcS|--warning-gw-current-session <integer>
+ -wGcS, --warning-gw-current-session <integer>
     Set if you want to enable Warning on Gateway Server current sessions.
     This will overwrite -w --warning
- -cGcS|--critical-gw-current-session <integer>
+ -cGcS, --critical-gw-current-session <integer>
     Set if you want to enable Critical on Gateway Server current sessions.
     This will overwrite -c --critical
- -wCce|--warning-cs-cert-expire <integer>
+ -wCce, --warning-cs-cert-expire <integer>
     Set if you want to warn on Connection Server Certificate expire. (Days)
- -cCce|--critical-cs-cert-expire <integer>
+ -cCce, --critical-cs-cert-expire <integer>
     Set if you want to critical on Connection Server Certificate expire. (Days)
  -s, --silent
     Silent all extra output
@@ -117,6 +122,7 @@ Options:
     Print extra Information
 
 Example: ${PROGNAME} -H <hostname> -P <PASSWORD> -D <DOMAIN> -A -w 50 -c 100 -v
+
 
 EOM
 }
@@ -133,7 +139,7 @@ while [[ -n "${1}" ]]; do
                 print_revision "${PROGNAME}" "${REVISION}"
                 exit ${STATE_OK}
                 ;;
-        -H|--host)
+        -H|--host|--hostname)
                 shift
                 api_host="${1}"
                 ;;
